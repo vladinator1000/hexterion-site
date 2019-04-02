311bdb4ca7c5aa87801ff0d322f58e35bba26e88
Vlady Veselinov <vladkodmc@gmail.com>
2019-04-02 19:59:45 +0100

    try to fix huge icons on initial load

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -2,7 +2,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faSteam, faYoutube } from "@fortawesome/free-brands-svg-icons"
-import { faNewspaper } from "@fortawesome/free-regular-svg-icons"
+import { faNewspaper, faGamepad } from "@fortawesome/free-regular-svg-icons"
 
 function getIcon(name = "") {
   switch (name.toLowerCase()) {
@@ -12,11 +12,19 @@ function getIcon(name = "") {
       return faYoutube
     case "newspaper":
       return faNewspaper
+    default:
+      return faGamepad
   }
 }
 
-export default function Icon(props) {
-  return <FontAwesomeIcon className="shadow" icon={getIcon(props.name)} />
+export default function Icon({ name, style }) {
+  return (
+    <FontAwesomeIcon
+      style={{ maxWidth: 128, height: "auto", ...style }}
+      className="shadow"
+      icon={getIcon(name)}
+    />
+  )
 }
 
 Icon.propTypes = {
