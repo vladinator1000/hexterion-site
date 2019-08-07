69496054bca6f998d370becab61f0340fd1a4b33
Vlady Veselinov <vladkodmc@gmail.com>
2019-08-07 21:27:07 +0100

    add description meta for privacy

--- src/pages/privacy.js
+++ src/pages/privacy.js
@@ -5,16 +5,18 @@ import Logo from "../components/Logo"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+const copy = "We (Hexterion) do not store or collect user data."
+
 export default function Privacy() {
   return (
     <Layout>
-      <SEO title="Privacy" keywords={["rebound", "dodgeball", "privacy"]} />
+      <SEO title="Privacy" description={copy} keywords={["rebound", "dodgeball", "privacy"]} />
       <Logo />
 
       <section className="card">
         <h1 style={{ marginRight: "0.618rem" }}>Privacy</h1>
         <p style={{ textAlign: "center" }}>
-          We (Hexterion) do not store or collect user data.
+          {copy}
         </p>
       </section>
       <Link to="/">Back to Home</Link>
