10d44625775d14f2f31322a5aed14b94c8c5048d
Vlady Veselinov <vladislav.veselinov@fanduel.com>
2019-09-23 23:16:46 +0100

    add external link

--- src/pages/index.js
+++ src/pages/index.js
@@ -43,7 +43,7 @@ const IndexPage = () => (
         <CallToAction to="/press/">
           <Icon name="newspaper" /> Press Kit
         </CallToAction>
-        <CallToAction to="https://discordapp.com/invite/Fzq8FH">
+        <CallToAction external to="https://discordapp.com/invite/Fzq8FH">
           <Icon name="discord" /> Discord
         </CallToAction>
       </div>
